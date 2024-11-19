9f1348f9d7a1dc721ae571c484da85637075af9f
ManishMandrai <manishkumarmandrai@gmail.com>
2024-11-19 14:58:33 +0530

    .

--- code.js
+++ code.js
@@ -1,6 +1,16 @@
-let a = 6;
-let b = 5;
+// let a = 6;
+// let b = 5;
 
-let cond1 = a > b;
-let cond2 = a === 5;
-console.log("cond1 && cond2 = ", cond1 && cond2)
+// let cond1 = a > b;
+// let cond2 = a === 5;
+// console.log("cond1 && cond2 = ", cond1 && cond2)
+
+
+let age = 15;
+
+if (age > 18){
+    console.log("You can vote");
+}else{
+    console.log("You can't vote");
+    
+}
